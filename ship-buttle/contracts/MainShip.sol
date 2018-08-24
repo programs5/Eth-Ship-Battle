98eaebe2d06338f3d22a0d9c3eb6469e1a07e3b8
pavel.malygin <pavel.malygin@nexign-systems.com>
2018-08-24 17:38:21 +0300

    240818_1738

--- ship-buttle/contracts/MainShip.sol
+++ ship-buttle/contracts/MainShip.sol
@@ -2,29 +2,40 @@ pragma solidity ^0.4.24;
 
 contract MainShip {
     address public owner;
+
     uint[] coCount;
 
+    // очередь ожидающих начала игры 
+    GamerInitData[] waitingQueue;
+
     struct GamerInitData {
-        uint stateHash;
-        uint beat;        
+        address gamer;
+        uint stateHash; // хешь от суммы цифр которые обозначают данные по каждому кораблю игрока
+        uint beat;      // размер ставки игрока   
     }
 
-    mapping(address => GamerInitData) public gamerData;
+    mapping(address => GamerInitData[2]) public gamerData;
     
     // конструктор
     constructor() public {
         owner = msg.sender;
-
-        coCount.push(1024);
-        coCount.push(1023);
-        coCount.push(1022);
     }
-
-   //function IsPlayerExists() public constant returns(bool) {
-   //   return gamersQueue[msg.sender];
-   //}
-
+    
+    // инициализируем новое обращение от игрока
     function initNewGame(uint _stateHash, uint _beat) public payable {
+        require(_beat > 0, "Размер ставки должен быть больше нуля");
+
+        if(waitingQueue.length == 0){
+            // если не с кем играть, добавляем игрока в очередь
+            waitingQueue.push(GamerInitData(msg.sender,  _stateHash, _beat));
+        }
+        else{
+            // если в очереди уже есть игрок в статусе ожидания
+
+
+
+        }
+
         GamerInitData storage init;
 
         init.stateHash = _stateHash;
